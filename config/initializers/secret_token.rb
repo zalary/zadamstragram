fc84469e7ed2fae4cc8cfc37243f8d1415e4f9c2
Zalary Young <zalary@gmail.com>
2014-03-24 16:41:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zadamstagram::Application.config.secret_key_base = '766167f2ed4706df3f68bee79b32c6e45fbac0a8b1f8bcba358de930716ecb3a9734df0b1e8130acacae706f6cbcb0d5f7d4b273c372f0c53b58385f471107fc'
