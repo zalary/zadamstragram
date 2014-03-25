a00ce2a9ad9dedadd69845f185e44fc2c13b0588
Zalary Young <zalary@gmail.com>
2014-03-25 17:29:22 -0500

    add like spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,1 +1,27 @@
+require 'spec_helper'
 
+describe Like do
+    context "likes" do
+
+    before do
+      @p = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg", user_id: 1)
+      @pid = @p.id
+      @l = Like.create(user_id: 1, post_id: @pid)
+    end
+
+    it "should have a user_id and a photo_id" do
+      expect(@p.like.user_id).to eq([1])
+      expect(@p.like.photo_id).to eq(@pid)
+      #expect(@c.text).to eq
+    end
+
+
+   pending it "has more than one comment" do
+
+      @c2 = Comment.create(text: "Totally Awesome", post_id: @pid)
+
+      expect(@p.comments).to eq([@c,@c2])
+      expect(@p.comments[1].text).to eq("Totally Awesome")
+    end
+  end
+end
