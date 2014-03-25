650e280d5886afd53bdbe56feee4b914298280d3
Adam Shukur <adamshukur@gmail.com>
2014-03-25 14:58:30 -0500

    Add validation and class_name to user.rb model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :follower
-  belongs_to :followed
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  validates :follower_id, :followed_id, presence: true
 end
