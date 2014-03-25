81d18269b564b56041d7d59ff171a4074c2f075a
shukbot <adamshukur@gmail.com>
2014-03-25 16:28:29 -0500

    able to make a comment on a post

---
+++ spec/models/comments_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Comments do
+  pending context "comments" do
+    it "has one comment" do
+
+    end
+
+    it "has more than one comment" do
+
+    end
+
+  end
+end
