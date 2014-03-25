70c2f8f3b9c899af9ae4e17fe9c2d7df25c394e4
shukbot <adamshukur@gmail.com>
2014-03-25 15:13:16 -0500

    add scoping methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
         :confirmable, :omniauthable
+
   has_many :relationships, foreign_key: "follower_id"
   has_many :followed_users, through: :relationships, source: :followed
 
-
-
-
-
-
-
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationships"
   has_many :followers, through: :reverse_relationships, source: :follower
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
 
 end
