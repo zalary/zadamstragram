8a6d1c1b8ba2d0dad8bfe5cdec42f72ba49650e2
shukbot <adamshukur@gmail.com>
2014-03-25 15:59:16 -0500

    Pass our first tests! Yay

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,16 +3,19 @@ require 'spec_helper'
 describe Post do
 
   context "image urls" do
+    
     before do
-      p = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg", user_id: 1)
-      p2 = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg", pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg", user_id: 2)
+      @p = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg")
     end
     it "has one image" do
-      expect(p.pic_url).to eq("http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg")
+      expect(@p.pic_url).to eq("http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg")
     end
 
+    before do
+      @p2 = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg", pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg")
+    end
     it "cannot have more than one" do
-      expect(p2.pic_url).to eq("http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg")
+      expect(@p2.pic_url).to eq("http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg")
     end
   end
 
