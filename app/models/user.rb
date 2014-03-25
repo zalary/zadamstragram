fdf4f384bc96e9a6a2707e17fa434a9ec73b278c
Adam Shukur <adamshukur@gmail.com>
2014-03-25 14:43:12 -0500

    Update model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :relationships, foreign_key: "follower_id"
+  has_many :followed_users, through: :relationships, source: :followed
+
+
+
+
+
+
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationships"
+  has_many :followers, through: :reverse_relationships, source: :follower
+
 end
