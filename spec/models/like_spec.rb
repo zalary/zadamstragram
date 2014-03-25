31f9eb03253a6c198d815d19eeb8ec0bfc172a75
Zalary Young <zalary@gmail.com>
2014-03-25 17:18:22 -0500

    like spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Like do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "likes" do
+
+    before do
+      @p = Post.create(pic_url: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg", user_id: 1)
+      @pid = @p.id
+      @c = Like.create(user_id: 1, post_id: @pid)
+    end
+
+    it "has one comment" do
+      expect(@p.comments).to eq([@c])
+      expect(@p.comments[0].text).to eq("Awesome")
+      #expect(@c.text).to eq
+    end
+
+
+   pending it "has more than one comment" do
+
+      @c2 = Comment.create(text: "Totally Awesome", post_id: @pid)
+
+      expect(@p.comments).to eq([@c,@c2])
+      expect(@p.comments[1].text).to eq("Totally Awesome")
+    end
+  end
 end
