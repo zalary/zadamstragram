cebbec6a8ffad9df6e927dc31687dd43f443ecbe
shukbot <adamshukur@gmail.com>
2014-03-27 17:15:58 -0500

    Work on ajax comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def create
+    @comment = Comment.new(comment_params)
 
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'was saved' }
+        format.json { render action: 'show', status: :created, location: @comment }
+        format.js {}
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
@@ -10,6 +23,10 @@ class CommentsController < ApplicationController
       @user = User.find(params[:id])
       @comment = Comment.new
       @post = Post.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:post_id, :text)
     end
 
 end
---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :post_id, :text
