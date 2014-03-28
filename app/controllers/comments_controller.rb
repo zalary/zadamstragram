f50728b840b3083bfbd1c7b1944111e642561e7c
shukbot <adamshukur@gmail.com>
2014-03-28 14:52:29 -0500

    Add a working comment form YAY

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,14 +19,7 @@ class CommentsController < ApplicationController
   private
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-      @comment = Comment.new
-      @post = Post.find(params[:id])
-    end
-
     def comment_params
       params.require(:comment).permit(:post_id, :text, :user_id)
     end
-
 end
