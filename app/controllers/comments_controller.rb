18dd76e54e766c99e8b0fd628b94ef29fb76f2ed
shukbot <adamshukur@gmail.com>
2014-03-28 16:53:02 -0500

    Almost have comment refreshing working... it's complicated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :create]
 
   def create
     @comment = Comment.new(comment_params)
@@ -7,8 +7,8 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'was saved' }
-        format.json { render action: 'show', status: :created, location: @comment }
-        format.js {}
+        format.json { render json: @comment, status: :created }
+        # format.js { }
       else
         format.html { render action: 'new' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -21,5 +21,9 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def comment_params
       params.require(:comment).permit(:post_id, :text, :user_id)
+    end
+
+    def set_user
+      @user = current_user
     end
 end
